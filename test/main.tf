f77b9b9a636fb377a9e6c4ec437b1ce98dc94936
Corey Hemminger <hemminger@hotmail.com>
2022-05-24 11:31:43 -0500

    test4
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- test/main.tf
+++ test/main.tf
@@ -1,4 +1,9 @@
 # Test github repo module
+provider "github" {
+  owner = "Stromweld"
+  token = var.github_token
+}
+
 module "Test" {
   source = "../"
 
---
+++ test/variables.tf
@@ -0,0 +1,5 @@
+variable "github_token" {
+  type        = string
+  description = "Token to authenticate to github"
+  sensitive   = true
+}
